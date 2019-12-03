8762a71b95a39aa05395591ae3fe4d214bf7aee8
Abid Rehman <arehma21@illinois.edu>
2019-12-03 17:26:47 -0600

    Final Project

---
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -0,0 +1,33 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+        Button takeDamage = findViewById(R.id.damage);
+        takeDamage.setOnClickListener(unused -> takeDamageClicked());
+
+    }
+    private void takeDamageClicked() {
+        LinearLayout damageparent = findViewById(R.id.DamageParent);
+        View damageChunk = getLayoutInflater().inflate(R.layout.damage_chunk,
+                damageparent, false);
+        //TextView dmgType = damageChunk.findViewsWithText(R.id.damageType);
+
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button newGame = findViewById(R.id.NewGame);
+        newGame.setOnClickListener(unused -> newGameclicked());
+
+
+    }
+    private void newGameclicked() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }
+}
