526519209eb7b4640fe7922e56bc12014a3f17cf
Abid Rehman <arehma21@illinois.edu>
2019-12-08 01:09:53 -0600

    Final Project, small update. Game Activity has buttons, worked on it.

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -8,29 +8,79 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameActivity extends AppCompatActivity {
+import org.json.JSONObject;
+import com.google.gson.JsonObject;
 
-    private int HitPoints;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date.*;
+//import com.google.gson.JsonObject;
+
+public class GameActivity extends AppCompatActivity {
+    private ImageView diceImage = findViewById(R.id.diceImage);
+    private boolean logRolls = false;
+    private ArrayList<JsonObject> log = new ArrayList<JsonObject>();
+    private int lastRoll;
+    private Timestamp timeLastRoll;
+    private String rollCheck;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
-        Button takeDamage = findViewById(R.id.damage);
-        takeDamage.setOnClickListener(unused -> takeDamageClicked());
+        Button d4 = findViewById(R.id.d4Game);
+        Button d6 = findViewById(R.id.d6Game);
+        Button d8 = findViewById(R.id.d8Game);
+        Button d10 = findViewById(R.id.d10Game);
+        Button d12 = findViewById(R.id.d12Game);
+        Button d20 = findViewById(R.id.d20Game);
+        d4.setOnClickListener(unused -> d4clicked());
+    }
+    private void d4clicked() {
+        int rollNumber = (int) ((Math.random() * 4) + 1);
+        lastRoll = rollNumber;
+        if (rollNumber == 1) {
+            diceImage.setImageResource(R.drawable.dice4_1);
+        }
+        if (rollNumber == 2) {
+            diceImage.setImageResource(R.drawable.dice4_2);
+        }
+        if (rollNumber == 3) {
+            diceImage.setImageResource(R.drawable.dice4_3);
+        }
+        if (rollNumber == 4) {
+            diceImage.setImageResource(R.drawable.dice4_4);
+        }
+        Timestamp currentTimeStamp = new Timestamp(System.currentTimeMillis());
+        timeLastRoll = currentTimeStamp;
+        if (logRolls) {
+            JsonObject diceRoll = new JsonObject();
+            diceRoll.addProperty("Number", rollNumber);
+            diceRoll.addProperty("Time", currentTimeStamp.toString());
+            diceRoll.addProperty("Check", rollCheck);
+        }
+    }
+    private void d6clicked() {
 
     }
-    private void takeDamageClicked() {
-        LinearLayout damageparent = findViewById(R.id.DamageParent);
-        View damageChunk = getLayoutInflater().inflate(R.layout.damage_chunk,
-                damageparent, false);
-        TextView dmgType = damageChunk.findViewById(R.id.damageType);
-        TextView amount = damageChunk.findViewById(R.id.amount);
+    private void d10clicked() {
+
     }
+    private void d12clicked() {
+
+    }
+    private void d20clicked() {
+
+    }
+
+
 }
