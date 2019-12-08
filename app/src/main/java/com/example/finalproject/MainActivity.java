185246ad8f4c6479f44a03c833584da4f5eab7e7
Abid Rehman <arehma21@illinois.edu>
2019-12-07 22:52:46 -0600

    Final Project, small update.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView D4;
+    private ImageView D6;
+    private ImageView D10;
+    private ImageView D12;
+    private int numDice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +20,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button newGame = findViewById(R.id.NewGame);
         newGame.setOnClickListener(unused -> newGameclicked());
+        Button rollD4 = findViewById(R.id.rollD4);
+        D4 = findViewById(R.id.D4);
+        rollD4.setOnClickListener(unused -> rollD4clicked());
+
 
     }
+
     private void newGameclicked() {
         Intent intent = new Intent(this, GameActivity.class);
 
         startActivity(intent);
-        
 
     }
+    private void rollD4clicked() {
+        int rollNumber = (int) ((Math.random() * 4) + 1);
+        if (rollNumber == 1) {
+            D4.setImageResource(R.drawable.dice4_1);
+        }
+        if (rollNumber == 2) {
+            D4.setImageResource(R.drawable.dice4_2);
+        }
+        if (rollNumber == 3) {
+            D4.setImageResource(R.drawable.dice4_3);
+        }
+        if (rollNumber == 4) {
+            D4.setImageResource(R.drawable.dice4_4);
+        }
+    }
+
+
 }
